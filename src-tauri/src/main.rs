d902e646d2cbf37687809fd97c8fffed31cb077a
Corban Thompson <corbant25@gmail.com>
2022-04-29 14:21:16 -0700

    Working on serial port stuff

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -2,9 +2,33 @@
   all(not(debug_assertions), target_os = "windows"),
   windows_subsystem = "windows"
 )]
+use std::time::Duration;
+
+struct ActivePort(String);
 
 fn main() {
-  tauri::Builder::default()
-    .run(tauri::generate_context!())
-    .expect("error while running tauri application");
+  	tauri::Builder::default()
+		.run(tauri::generate_context!())
+		.expect("error while running tauri application");
+}
+
+
+#[tauri::command]
+fn connect_to_station(state: tauri::State<ActivePort>) {
+	let open_ports = serialport::available_ports().unwrap();
+	for port in open_ports {
+		let port_name = port.port_name.to_string();
+		let mut port = match serialport::new(port_name, 9600).timeout(Duration::from_millis(10)).open() {
+			Ok(port) => port,
+			Err(_) => continue
+		};
+		port.write("identify".as_bytes()).unwrap();
+
+		let mut serial_buf: Vec<u8> = vec![0; 32];
+		port.read(serial_buf.as_mut_slice());
+		if(String::from_utf8(serial_buf)).eq("BaseStation") {
+			state.0 = port_name;
+		};
+
+	}
 }
--- src/routes/Launch.svelte
+++ src/routes/Launch.svelte
@@ -1,13 +1,37 @@
 <script>
 	import { Line } from 'svelte-chartjs'
+	import {enUS} from 'date-fns/locale'
+	import 'chartjs-adapter-date-fns';
 </script>
 
 
 <div>
-	<Line data={{
+	<!--<Line data={{
 		datasets: [{
 			label: "Altitude",
-			data: [1, 5, 1, 7, 5],
+			data: [
+				{
+					x: 0,
+					y: 0,
+				},
+				{
+					x: 10,
+					y: 5,
+				},
+				{
+					x: 50,
+					y: 8,
+				},
+				{
+					x: 120,
+					y: 9,
+				},
+				{
+					x: 130,
+					y: 10,
+				},
+
+			],
 			backgroundColor: "rgba(255, 99, 132, 0.2)",
 			borderColor: "rgba(255, 99, 132, 1)",
 			borderWidth: 1
@@ -17,11 +41,21 @@
 		responsive: true,
 		scales: {
 			x: {
+				type: 'time',
+				time: {
+					unit: 'seconds',
+					parser: 't.SS'
+				},
 				ticks: {
 					source: 'auto'
-				}
+				},
+			},
+			adapters: {
+				locale: enUS
 			}
 		}
-	}}/>
+	}}/>-->
+
+	
 
 </div>
