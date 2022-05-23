cbc1be143436a72fa93a87ec840c258b716a0ee4
IIVioletKingII <samdepoule@gmail.com>
2022-05-23 14:02:05 -0700

    made the thread execution part of the frontend

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -2,7 +2,7 @@
   all(not(debug_assertions), target_os = "windows"),
   windows_subsystem = "windows"
 )]
-use std::{time::Duration, sync::Mutex, thread};
+use std::{time::Duration, sync::Mutex};
 
 use tauri::{Window};
 
@@ -17,43 +17,35 @@ struct Payload {
 fn main() {
   	tauri::Builder::default()
 	  	.manage(ActivePort(String::new().into()))
-		.invoke_handler(tauri::generate_handler![start_connection_service])
+		.invoke_handler(tauri::generate_handler![check_station_connection])
 		.run(tauri::generate_context!())
 		.expect("error while running tauri application");
 }
 
-#[tauri::command]
-fn start_connection_service(state: tauri::State<ActivePort> , window: Window) {
-	thread::spawn( ||  {
-		//check port connection status every second
-		loop {
-			check_station_connection(state, window);
-			thread::sleep(Duration::from_millis(1000));
-		}
-	});
-}
 
+#[tauri::command]
 fn check_station_connection(state: tauri::State<ActivePort>, window: Window) {
 	//if we don't already know which port the station is connected to
-	if state.0.lock().unwrap().is_empty() {
+	let active_port = &state.0;
+	if active_port.lock().unwrap().is_empty() {
 		let open_ports = serialport::available_ports().unwrap();
 		for port in open_ports {
 			//if the port is the one we want
 			if verify_connection(port.port_name.to_string()) {
-				let mut active_port = state.0.lock().unwrap();
-				*active_port = port.port_name.to_string();
+				let mut port_state = active_port.lock().unwrap();
+				*port_state = port.port_name.to_string();
 				//let frontend know that we have a connection
-				window.emit("connection-status", Payload { status: true, device: state.0.lock().unwrap().to_string() });
+				window.emit("connection-status", Payload { status: true, device: active_port.lock().unwrap().to_string() }).unwrap();
 			}
 		}
 		
 	}
 	//else we already know which device it is and we want to verify it is still connected
 	else {
-		if !verify_connection(state.0.lock().unwrap().to_string()) {
+		if !verify_connection(active_port.lock().unwrap().to_string()) {
 			//if the device is no longer connected, remove it as the active device and let the frontend know that we are no longer connected
-			state.0.lock().unwrap().clear();
-			window.emit("connection-status", Payload { status: false, device: String::new() });
+			active_port.lock().unwrap().clear();
+			window.emit("connection-status", Payload { status: false, device: String::new() }).unwrap();
 		}
 	}
 }
@@ -66,10 +58,10 @@ fn verify_connection(port: String) -> bool {
 			return false;
 		} 
 	};
-	port.write("ping".as_bytes()).unwrap();
+	port.write("arm".as_bytes()).unwrap();
 	let mut serial_buf: Vec<u8> = vec![0; 32];
-	port.read(serial_buf.as_mut_slice());
-	if String::from_utf8(serial_buf).unwrap().eq("pong") {
+	port.read(serial_buf.as_mut_slice()).unwrap();
+	if String::from_utf8(serial_buf).unwrap().eq("armed") {
 		return true;
 	};
 	return false;
